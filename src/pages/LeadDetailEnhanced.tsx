5162f40e6ab05fd76ef08bd76dadd2da437f4237
Keisse Brandão <keissebrandao@gmail.com>
2026-09-16 02:06:46 -0300

    Remove duplicate accumulated stage information block

--- src/pages/LeadDetailEnhanced.tsx
+++ src/pages/LeadDetailEnhanced.tsx
@@ -1,22 +1,5 @@
-import { useParams } from "react-router-dom";
 import LeadDetail from "./LeadDetail";
-import { LeadStageInformation } from "@/components/admin/LeadStageInformation";
 
-const LeadDetailEnhanced = () => {
-  const { id } = useParams<{ id: string }>();
-
-  return (
-    <>
-      <LeadDetail />
-      {id && (
-        <div className="px-4 pb-6">
-          <div className="max-w-5xl mx-auto">
-            <LeadStageInformation leadId={id} />
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
+const LeadDetailEnhanced = () => <LeadDetail />;
 
 export default LeadDetailEnhanced;
